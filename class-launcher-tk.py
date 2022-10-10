0f768ac7208f0e54003e8d2a2b991abf853b85f3
dlordmdia <shnuffi2.02@gmail.com>
2022-10-10 13:12:38 +0200

    Chanched links in english and geography

--- class-launcher-tk.py
+++ class-launcher-tk.py
@@ -57,13 +57,13 @@ label_style.configure("My.TLabel", background="white")
 class_list = [
     {"name": "Català", "id": "NTQ2NzkyMzY3NDcy"},
     {"name": "Castellano", "id": "NTQ2OTAwOTYzNDkx"},
-    {"name": "English", "id": "https://google.com"},  # LINK NOT DEFINED!
+    {"name": "English", "id": "NTI3MTU2NzY4Mjg4"},  # LINK DEFINED?
     {"name": "Mates", "id": "NTQ1ODQ1NjA3MTA3"},
     {"name": "TIC", "id": "NTQ2MjkyMDE4MDI5"},
     {"name": "Tecnologia", "id": "NTQ2OTY2NTc4OTAx"},
     {"name": "Economia", "id": "NTQ3MjUzNjgxNzIw"},
     {"name": "Educació Física", "id": "NTQ3MjE2MzkzOTA2"},
-    {"name": "Geografia", "id": "NTQ3MDA3OTU4Owindowx"},
+    {"name": "Geografia", "id": "NTQ3MDA3OTU4OTkx"},
     {"name": "Tutoria", "id": "NTQ1NjczNjg1MDMz"},
     {"name": "Valors", "id": "NTQ1ODM0Njg1NzE0"},
 
